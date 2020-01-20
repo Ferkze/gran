5ae81d2be1834f2248d7db6e309846c14745f274
Fabio <ferkemartins@gmail.com>
2020-01-20 12:44:36 -0300

    Added typescript refactoring

---
+++ src/views/Auth.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center row wrap>
+            <v-flex xs12 sm8 md6>
+                <v-card>
+                    <v-card-title>
+                        Entre com seu email
+                    </v-card-title>
+                    <v-divider/>
+                    <v-card-text>
+                        <v-text-field label="Email"/>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>

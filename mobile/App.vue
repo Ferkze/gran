8b8402bc7eeaf0e8841d78c72c52490c7241ad83
fabio-devins <ferkemartins@gmail.com>
2020-03-24 08:11:00 -0300

    Adicionado diretório mobile com projeto em vue-native e expo

---
+++ mobile/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createStackNavigator,
+} from "vue-native-router";
+
+import HomeScreen from './screens/HomeScreen.vue'
+import LoginScreen from './screens/LoginScreen.vue'
+
+const StackNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Login: LoginScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator },
+  data: () => ({
+    title: 'Finance App',
+    text: ''
+  }),
+  methods: {
+      onPressLearnMore: function() {
+          alert(this.text)
+      }
+  },
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ mobile/screens/HomeScreen.vue
@@ -0,0 +1,63 @@
+<template>
+  <view class="container">
+    <status-bar
+        background-color="blue"
+        bar-style="light-content"
+    />
+    <image
+      :style="{width: 50, height: 50}"
+      :source="{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}"
+    />
+    <text class="text-color-primary">{{ title }}</text>
+    <text-input
+        :style="{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}"
+        v-model="text"
+      />
+      <button
+        :on-press="onPressLearnMore"
+        title="Learn More"
+        color="#841584"
+        accessibility-label="Learn more about this purple button"
+      />
+      <button
+        :on-press="navigateToLogin"
+        title="Login"
+        color="#841584"
+        accessibility-label="Login navigation button"
+      />
+  </view>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'Finance Home Page',
+    text: ''
+  }),
+  methods: {
+    onPressLearnMore: function() {
+      alert(this.text)
+    },
+    navigateToLogin: function() {
+      this.navigation.navigate('Login')
+    },
+  },
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>

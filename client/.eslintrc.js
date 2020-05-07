b7d177de127a1ad2910951b89c724022fb4def2a
Ferkze <ferkemartins@gmail.com>
2020-05-07 12:18:58 -0300

    Adicionado correção de import de interfaces

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -9,14 +9,9 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: ['error', 'never'],
     quotes: ['error', 'single'],
-    indent: ['error', 2],
-    'no-unused-vars': [
-      1,
-      {
-        ignoreSibilings: true,
-        argsIgnorePattern: 'res|next|^err'
-      }
-    ]
+    indent: ['error', 2, { 'SwitchCase': 1 }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
   },
   parserOptions: {
     ecmaVersion: 2018,

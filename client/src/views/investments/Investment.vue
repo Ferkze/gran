3cd9bb8b05841a962003a6e637e9c4f12e97120c
Ferkze <ferkemartins@gmail.com>
2020-06-01 21:37:15 -0300

    Adicionado getters para diferentes tipos de investimentos

--- client/src/views/investments/Investment.vue
+++ client/src/views/investments/Investment.vue
@@ -52,16 +52,30 @@ export default class Investment extends Vue {
     { name: 'Renda Fixa', total: 4980.87, color: 'blue', totalAssets: 2, monthResult: 10.37 }
   ]
   investments = [
-    { type: 'Acoes', name: 'PETR4', title: 'Ações Petrobrás', totalAsset: 2570.99 },
-    { type: 'Acoes', name: 'VVAR3', title: 'Ações Via Varejo', totalAsset: 2570.99 },
-    { type: 'Acoes', name: 'ITUB3', title: 'Ações Itaú Unibanco', totalAsset: 2570.99 },
-    { type: 'Acoes', name: 'TOTS3', title: 'Ações TOTVS', totalAsset: 2570.99 },
-    { type: 'Acoes', name: 'VIVA3', title: 'Ações Vivara', totalAsset: 2570.99 },
-    { type: 'Acoes', name: 'TAEE11', title: 'Ações Taesa', totalAsset: 2570.99 },
-    { type: 'Fundos', name: 'BCFF11', title: 'Fundo Imobiliário Fundo de Fundos', totalAsset: 2570.99 },
-    { type: 'Fundos', name: 'KNRI11', title: 'Fundo Imobiliário Kinea Renda Imobiliária', totalAsset: 2570.99 },
-    { type: 'Fundos', name: 'MXRF11', title: 'Fundo Imobiliário Maxi Renda', totalAsset: 2570.99 }
+    { type: 'Acoes', name: 'PETR4', title: 'Ações Petrobrás', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Acoes', name: 'VVAR3', title: 'Ações Via Varejo', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Acoes', name: 'ITUB3', title: 'Ações Itaú Unibanco', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Acoes', name: 'TOTS3', title: 'Ações TOTVS', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Acoes', name: 'VIVA3', title: 'Ações Vivara', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Acoes', name: 'TAEE11', title: 'Ações Taesa', totalAsset: 2570.99, quantity: 120 },
+    { type: 'Fundos', name: 'BCFF11', title: 'FII Fundo de Fundos', totalAsset: 2570.99, quantity: 120 },
+    {
+      type: 'Fundos',
+      name: 'KNRI11',
+      title: 'FII Kinea Renda Imobiliária',
+      totalAsset: 2570.99,
+      quantity: 120
+    },
+    { type: 'Fundos', name: 'MXRF11', title: 'FII Maxi Renda', totalAsset: 2570.99, quantity: 120 }
   ]
+
+  get fundsInvestments() {
+    return this.investments.filter(i => i.type == 'Fundos')
+  }
+
+  get sharesInvestments() {
+    return this.investments.filter(i => i.type == 'Acoes')
+  }
 
   get totalInvested() {
     return this.investments.reduce((a, c) => a + c.totalAsset, 0)

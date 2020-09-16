530936518edf327386fac2d5bdcc8de7093ff227
Ferkze <ferkemartins@gmail.com>
2020-09-15 22:28:14 -0300

    Adição de novas rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import passport from 'passport'
 import accountController from './controllers/account.controller'
 import authController from './controllers/auth.controller'
+import budgetController from './controllers/budget.controller'
 import categoryController from './controllers/category.controller'
+import groupController from './controllers/group.controller'
 import indexController from './controllers/index.controller'
 import institutionController from './controllers/institution.controller'
+import transactionController from './controllers/transaction.controller'
 import userController from './controllers/user.controller'
 import authenticationMiddleware from './middlewares/authentication.middleware'
 
@@ -18,26 +20,26 @@ router.post('/auth/current', authController.current)
 
 // Dados do usuário, precisa estar autenticado para acesser os dados de usuários
 // router.get('/users', userController.index)
-// router.get('/user/:id', userController.find)
+router.get('/user/:userId', userController.readById)
 // router.post('/users', userController.store)
 // router.delete('/user/:userId', userController.delete)
 router.put('/user', authenticationMiddleware.userRequired, userController.update)
 
 // Precisam de autenticação, para que as transações do usuário sejam encontradas
-// router.get('/transactions', authenticationMiddleware.userRequired, transactionController.index)
+router.get('/transactions', authenticationMiddleware.userRequired, transactionController.readByUser)
 // router.get('/transaction/:transactionId', authenticationMiddleware.userRequired, transactionController.find)
-// router.put('/transaction/:transactionId', authenticationMiddleware.userRequired, transactionController.update)
+router.post('/transactions', authenticationMiddleware.userRequired, transactionController.create)
+router.put('/transaction/:transactionId', authenticationMiddleware.userRequired, transactionController.update)
 // router.post('/transaction', authenticationMiddleware.userRequired, transactionController.store)
-// router.post('/transactions', authenticationMiddleware.userRequired, transactionController.store)
 // router.delete('/transaction/:transactionId', authenticationMiddleware.userRequired, transactionController.remove)
 
 // Precisam de autenticação, para que as contas do usuário sejam encontradas
 router.get('/accounts', authenticationMiddleware.userRequired, accountController.index)
-// router.delete('/accounts/:accountId', authenticationMiddleware.userRequired, accountController.delete)
-// router.get('/accounts/:accountId/transactions', authenticationMiddleware.userRequired, transactionController.findByAccount)
-// router.post('/accounts', authenticationMiddleware.userRequired, accountController.store)
-// router.put('/account/:accountId', authenticationMiddleware.userRequired, accountController.put)
+router.post('/accounts', authenticationMiddleware.userRequired, accountController.create)
+router.put('/account/:accountId', authenticationMiddleware.userRequired, accountController.update)
+router.delete('/accounts/:accountId', authenticationMiddleware.userRequired, accountController.delete)
 // router.get('/account/:accountId/balance', authenticationMiddleware.userRequired, accountController.balance)
+// router.get('/accounts/:accountId/transactions', authenticationMiddleware.userRequired, transactionController.findByAccount)
 
 // Não precisam de autenticação, essas instituições são genéricas para o app
 router.get('/institutions', institutionController.index)
@@ -48,8 +50,17 @@ router.get('/institutions', institutionController.index)
 // router.delete('/institutions', institutionController.deleteMany)
 
 // Não precisam de autenticação, uma vez que o usuário utiliza as categorias padrões
-router.get('/categories', authenticationMiddleware.userRequired, categoryController.index)
+router.get('/categories', authenticationMiddleware.userRequired, categoryController.readByUser)
 // router.get('/category/:categoryId', authenticationMiddleware.userRequired, categoryController.find)
-// router.post('/categories', authenticationMiddleware.userRequired, categoryController.store)
+router.post('/categories', authenticationMiddleware.userRequired, categoryController.create)
+router.put('/categories/:categoryId', authenticationMiddleware.userRequired, categoryController.update)
+
+router.get('/budgets', authenticationMiddleware.userRequired, budgetController.readByUser)
+router.post('/budgets', authenticationMiddleware.userRequired, budgetController.create)
+router.put('/budgets/:budgetId', authenticationMiddleware.userRequired, budgetController.update)
+
+router.get('/groups', authenticationMiddleware.userRequired, groupController.readByUser)
+router.post('/groups', authenticationMiddleware.userRequired, groupController.create)
+router.put('/groups/:groupId', authenticationMiddleware.userRequired, groupController.update)
 
 export default router

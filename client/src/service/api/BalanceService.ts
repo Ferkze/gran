04f072ccea7d9f102c18bc2d1b2680ac4482bed5
Ferkze <ferkemartins@gmail.com>
2020-10-03 15:27:00 -0300

    Balance service

---
+++ client/src/service/api/BalanceService.ts
@@ -0,0 +1,28 @@
+import client from "./ApiService";
+
+export interface Balance {
+	start: Date
+	finish: Date
+	credits: number
+	debits: number
+	balance: number
+}
+
+interface Response {
+	balance: Balance
+	error?: string
+}
+
+class BalanceService {
+
+  async getBalance() {
+    const response = await client.get<Response>(`/balance`)
+    if (response.data.error) {
+      throw new Error(response.data.error)
+    }
+    return response.data.balance
+  }
+
+}
+
+export default new BalanceService()

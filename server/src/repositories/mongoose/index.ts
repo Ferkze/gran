c5d766b5d0325a7499bf9474e7c781639248dccf
Ferkze <ferkemartins@gmail.com>
2020-10-09 19:54:37 -0300

    Adicionado repositório de grupos

--- server/src/repositories/mongoose/index.ts
+++ server/src/repositories/mongoose/index.ts
@@ -5,11 +5,12 @@ import { Repositories } from '..'
 import categoryRepository from './category.repository'
 import budgetRepository from './budget.repository'
 import transactionRepository from './transaction.repository'
+import groupRepository from './group.repository'
 
 const repository: Repositories = {
 	account: accountRepository,
 	budget: budgetRepository,
-	group: null,
+	group: groupRepository,
 	category: categoryRepository,
 	institution: institutionRepository,
 	user: userRepository,

a39b9acbc9fa7feb29fc5109090825d6c91bce3b
Ferkze <ferkemartins@gmail.com>
2020-10-12 21:05:25 -0300

    remoção do find and modify do mongodb

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,15 +4,15 @@ import debug from 'debug'
 const log = debug('app:mongoose')
 
 export default async (): Promise<void> => {
-	log(`Conectando ao MongoDB usando a URL: ${process.env.MONGODB_CONNECTION}`)
+	log(`Conectando ao MongoDB: ${process.env.MONGODB_CONNECTION}`)
 	try {
 		await mongoose.connect(process.env.MONGODB_CONNECTION, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useFindAndModify: true,
+			useFindAndModify: false,
 			useCreateIndex: true
 		})
-		log('Sucesso ao se conectar com o MongoDB')
+		log('Conectado ao MongoDB')
 	} catch (error) {
 		log('Erro ao se conectar com o MongoDB:', error)
 		throw new Error(error)

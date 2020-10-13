ca7a557e15136e85eb603dac5c718f726895c267
Ferkze <ferkemartins@gmail.com>
2020-10-13 00:25:50 -0300

    Adicionado base button

---
+++ client/src/components/base/Button.vue
@@ -0,0 +1,10 @@
+<template>
+	<v-btn color="primary" v-bind="$attrs" v-on="$listeners" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BaseButton extends Vue { }
+</script>

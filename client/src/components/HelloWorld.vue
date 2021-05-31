72d93cc8fbbf8c6c12dd69dc98a313d9cfed0161
Ferkze <ferkemartins@gmail.com>
2021-05-30 21:28:59 -0300

    Layout and branding changes

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -183,7 +183,10 @@
 
         <v-row justify="space-between">
           <v-col cols="3">
-            <p class="text-start mb-0 white--text">© 2021 granFinance</p>
+            <p class="text-start mb-0 white--text">
+              <span class="font-weight-regular">© 2021 gran</span>
+              <span class="font-weight-light">finance</span>
+            </p>
           </v-col>
           <v-col cols="9">
             <p class="text-end mb-0 white--text">Segurança no uso da internet | Termos de Uso e Política de Privacidade</p>
@@ -191,23 +194,6 @@
         </v-row>
       </v-container>
     </section>
-
-    <!-- <v-row class="text-center">
-			<v-col cols="12">
-				<v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
-			</v-col>
-
-			<v-col class="mb-4">
-				<h1 class="display-2 font-weight-bold mb-3">
-					{{ msg }}
-				</h1>
-				<p class="subheading font-weight-regular">
-					<router-link to="/entrar">
-						Register
-					</router-link>
-				</p>
-			</v-col>
-    </v-row>-->
   </v-container>
 </template>
 
--- client/src/components/base/GranLogo.vue
+++ client/src/components/base/GranLogo.vue
@@ -1,12 +1,12 @@
 <template>
-	<router-link
-		:to="{ name: 'Home' }"
-		aria-label="Gran Home Page"
-		class="d-flex align-center text--primary"
-		style="text-decoration: none;"
-		title="Gran Home Page"
-	>
-		<v-img
+  <router-link
+    :to="{ name: 'Home' }"
+    aria-label="Gran Home Page"
+    class="d-flex align-center text--primary"
+    style="text-decoration: none;"
+    title="Gran Home Page"
+  >
+    <v-img
       :src="require('@/assets/gran-logo.svg')"
       alt="Vuetify Logo"
       class="shrink mr-2"
@@ -14,18 +14,19 @@
       transition="scale-transition"
       width="40"
     />
-		<v-sheet class="display-1 hidden-sm-and-down mr-0 mr-md-4" color="transparent">
-			<span class="font-weight-light text-h5">gran</span><span class="font-weight-regular text-h5">Finance</span>
-		</v-sheet>
-	</router-link>
+    <v-sheet class="display-1 hidden-sm-and-down mr-0 mr-md-4 mb-2" color="transparent">
+      <span class="font-weight-regular text-h5">gran</span>
+      <span class="font-weight-light text-h5">finance</span>
+    </v-sheet>
+  </router-link>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-	name: 'BaseGranLogo',
-	inject: ['theme']
+  name: "BaseGranLogo",
+  inject: ["theme"],
 })
 export default class GranLogo extends Vue {}
 </script>
